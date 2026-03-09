26f9543cbdf3b13abfd24da59c1c79144a6e99be
Federico Frutos <fru.federico@gmail.com>
2026-03-09 19:52:34 -0300

    chore: remove unused files

--- install.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d D:\Proyectos\ofertita
-npm install > C:\Users\feder\AppData\Local\Temp\ofertita_install.txt 2>&1
-echo Exit: %ERRORLEVEL% >> C:\Users\feder\AppData\Local\Temp\ofertita_install.txt
--- setup.ps1
+++
@@ -1,3 +0,0 @@
-$env:PATH = "E:\Archivos de programas\nodejs;" + $env:PATH
-Set-Location "D:\Proyectos\ofertita"
-& "E:\Archivos de programas\nodejs\npm.cmd" run dev
